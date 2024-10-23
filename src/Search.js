9ee6972b99914c5c1039d46780b5b68fdd7f1c6b
Amanda <iulianoamanda@gmail.com>
2024-10-23 13:41:17 -0500

    uploaded from vscode

---
+++ src/Search.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Search(props) {
+  let [temp, setTemp] = useState(null);
+  let [city, setCity] = useState("");
+
+  function showTemp(response) {
+    setTemp(response.data.main.temp);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1eab6e5dea97d6e93fb2c4fd3560f12a&units=imperial`;
+    axios.get(URL).then(showTemp);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={updateCity} />
+        <input type="submit" value="search" />
+      </form>
+
+      {temp && <p>Temperature: {temp}°</p>}
+    </div>
+  );
+}
